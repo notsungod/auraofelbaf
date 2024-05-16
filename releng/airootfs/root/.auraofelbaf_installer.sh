da5defa4e194f259fe04ee8220736110dca51469
notsungod <fyodor@alyokhin.de>
2024-05-16 12:17:37 +0200

    add tmux plugin prefix I to install

--- releng/airootfs/root/.auraofelbaf_installer.sh
+++ releng/airootfs/root/.auraofelbaf_installer.sh
@@ -167,6 +167,7 @@ rm .bashrc
 /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showuntrackedfiles no
 git clone https://github.com/gpakosz/.tmux.git
 ln -s ".tmux/.tmux.conf" "$HOME/.config/tmux/tmux.conf"
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 exit
 EOF
 
